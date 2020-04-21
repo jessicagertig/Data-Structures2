7ebd78f12cc147419fa02db85098a1bb326b832a
Jessica Cooper <jessicagcooper@gmail.com>
2020-04-21 18:52:40 -0500

    Completed LRUCache, I know there is a better way.  But it works.

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,9 +8,11 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
-    def __init__(self, limit=10):
-        pass
-
+    def __init__(self, limit=3):
+        self.limit = limit
+        self.dll_cache = DoublyLinkedList()
+        self.storage_dict = dict()
+        self.size = 0
     """
     Retrieves the value associated with the given key. Also
     needs to move the key-value pair to the end of the order
@@ -17,7 +21,20 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key not in self.storage_dict:
+            return None
+        else:
+            value = self.storage_dict[key]
+            curr_node = self.dll_cache.head
+            while(curr_node is not None):
+                if curr_node.value[0] == key:
+                    node = curr_node
+                    self.dll_cache.move_to_front(node)
+                    curr_node = curr_node.next
+                else:
+                    curr_node = curr_node.next
+            return value
+
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +47,57 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.storage_dict:
+            curr_node = self.dll_cache.head
+            while(curr_node is not None):
+                if curr_node.value[0] == key:
+                    self.dll_cache.delete(curr_node)
+                    curr_node = curr_node.next
+                else:
+                    curr_node = curr_node.next
+            self.dll_cache.add_to_head((key, value))
+            self.storage_dict[key] = value
+        elif self.size < self.limit:
+            self.size += 1
+            ##add to head (key, value)
+            self.dll_cache.add_to_head((key, value))
+            ##add to dictionary
+            self.storage_dict[key] = value
+        elif self.size == self.limit: 
+            oldest_key = self.dll_cache.tail.value[0]
+            self.storage_dict.pop(oldest_key)
+            self.dll_cache.remove_from_tail()
+            self.dll_cache.add_to_head((key, value))
+            self.storage_dict[key] = value
+
+
+    
+attempt = LRUCache()
+attempt.set('item1', 'a')
+print(attempt.size)
+print("Head", attempt.dll_cache.head.value)
+attempt.set('item2', 'b')
+print(attempt.size)
+print(attempt.dll_cache.length)
+
+print(attempt.storage_dict)
+print("Head", attempt.dll_cache.head.value)
+print("Tail", attempt.dll_cache.tail.value)
+
+attempt.set('item2', 'c')
+print(attempt.size)
+print(attempt.storage_dict)
+print("Head", attempt.dll_cache.head.value)
+print("Tail", attempt.dll_cache.tail.value)
+
+attempt.set('item3', 'b')
+attempt.set('item4', 'z')
+print(attempt.size)
+print(attempt.storage_dict)
+print("Head", attempt.dll_cache.head.value)
+print("Tail", attempt.dll_cache.tail.value)
+attempt.get('item2')
+print(attempt.size)
+print(attempt.storage_dict)
+print("Head", attempt.dll_cache.head.value)
+print("Tail", attempt.dll_cache.tail.value)
